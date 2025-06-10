04bc067e021d2879163e0c55471369cde57b426a
Iuri Freire <iurifreire.18@gmail.com>
2025-06-10 20:50:42 -0300

    Correção de variavel de ambiente (API_URL) localizada em  userServiceAPI

--- frontend/src/services/userServiceAPI.js
+++ frontend/src/services/userServiceAPI.js
@@ -1,7 +1,7 @@
 import api from './axiosClient';
 import { formatarDataISO } from '../utils/dateUtils';
 
-const API_URL = 'http://localhost:3001/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
 
 export async function buscarReservas() {
     try {
